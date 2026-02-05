78329c72ce62040af2e69f1364c3221d1cb80268
Edouardform <edouard.form@proton.me>
2026-02-05 09:16:11 +0000

    Add files via upload

---
+++ PV.ps1
@@ -0,0 +1,15 @@
+#### Create directories ####
+$date = Get-Date -Format "dd_MM_yy"
+$parent = "Sav_$date"
+$directory = "C", "C\Appdata", "D", "Logiciel\GC", "Logiciel\CO", "Logiciel\PV", "Logiciel\PA", "Navigateur\chrome", "Navigateur\edge", "Navigateur\firefox"
+
+foreach ($d in $directory)
+{
+New-Item -ItemType Directory -name "$parent\$d"
+}
+
+
+
+
+
+
