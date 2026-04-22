675005cbe41be1bb116873f28c9f98b38c303697
Edouardform <edouard.form@proton.me>
2026-04-22 16:36:14 +0200

    Add files via upload

--- PV.ps1
+++ PV.ps1
@@ -1,13 +1,9 @@
 ﻿#### create directories ####
 $date = Get-Date -Format "dd_MM_yy"
 $parent = "Sav_$date"
-$directory = "C", "C\Appdata", "D", "Logiciel\GC", "Logiciel\CO", "Logiciel\PV", "Logiciel\PA", "Navigateur\chrome", "Navigateur\edge", "Navigateur\firefox"
+$directory = "C", "C\Appdata", "D", "Logiciel\GC", "Logiciel\CO", "Logiciel\PV", "Logiciel\PA\Troupo", "Logiciel\PA\GestTroupo", "Logiciel\PV\Geofolia", "Logiciel\PV\GestParcelles", "Navigateur\chrome", "Navigateur\edge", "Navigateur\firefox"
 
 foreach ($d in $directory)
 {
 New-Item -ItemType Directory -name "$parent\$d"
 }
-
-
-
-
